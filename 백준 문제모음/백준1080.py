b1c5d02a737467fb3cbcbc278013f90554034546
이건회 <rg970604@naver.com>
2022-01-10 15:03:48 +0900

    1/10

---
+++ 백준 문제모음/백준1080.py
@@ -0,0 +1,29 @@
+def change(x,y):
+  for i in range(x,x+3):
+    for j in range(y,y+3):
+      a[i][j] =1 - a[i][j]
+
+def check():
+  for i in range(n):
+    for j in range(m):
+      if a[i][j]!=b[i][j]:
+        return False
+  return True
+
+n,m=map(int,input().split())
+
+a=[list(map(int,list(input()))) for _ in range(n)]
+b=[list(map(int,list(input()))) for _ in range(n)]
+
+cnt=0
+
+for i in range(n-2):
+  for j in range(m-2):
+    if a[i][j] != b[i][j]:
+      change(i,j)
+      cnt+=1
+
+if check():
+  print(cnt)
+else:
+  print('-1')

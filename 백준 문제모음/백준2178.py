ab5467a9fe459af2aaaf9982e3b9cf710efa0e01
이건회 <rg970604@naver.com>
2022-01-14 02:43:47 +0900

    1/14

---
+++ 백준 문제모음/백준2178.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+n,m=map(int,input().split())
+graph=[list(map(int,list(input())))for _ in range(n)]
+
+dx,dy=[-1,1,0,0],[0,0,1,-1]
+
+def bfs(n,m):
+  queue=deque([])
+  queue.append([0,0])
+  while queue:
+    x,y=queue.popleft()
+    for i in range(4):
+      nx=x+dx[i]
+      ny=y+dy[i]
+      if nx<0 or nx>=n or ny<0 or ny>=m:
+        continue
+      if graph[nx][ny]==0:
+        continue
+      if graph[nx][ny]==1:
+        graph[nx][ny]=graph[x][y]+1
+        queue.append((nx,ny))
+  return graph[n-1][m-1]
+
+print(bfs(n,m))

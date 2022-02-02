110d92ac1c1714f8e896c7a927e477aa3cdbfa38
이건회 <rg970604@naver.com>
2022-02-02 18:04:40 +0900

    Create 백준14501런타임.py

---
+++ 백준 문제모음/백준14501런타임.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+n=int(input())
+#n일까지 벌 수 있는 최대수익을 저장
+d=[0]*16
+tplist=[[0,0]]
+for _ in range(n):
+  t,p=map(int,input().split())
+  tplist.append([t,p])
+
+for i in range(1,n+1):
+  x=tplist[i][0]-1
+  d[i]=max(d[i-1],d[i])
+  d[i+x] = max(d[i-1]+tplist[i][1],d[i+x])
+  
+print(d[n])

801cb3ee39478a137be9c092091edd2a72e62e1d
이건회 <rg970604@naver.com>
2022-03-04 02:47:32 +0900

    Create 백준9095.py

---
+++ 백준 문제모음/백준9095.py
@@ -0,0 +1,20 @@
+import sys
+
+input=sys.stdin.readline
+
+t=int(input())
+for _ in range(t):
+  n=int(input())
+  def dp(n):
+    d=[0]*12
+    d[0]=0
+    d[1]=1
+    d[2]=2
+    d[3]=4
+    
+    for i in range(4,n+1):
+      d[i]=d[i-1]+d[i-2]+d[i-3]
+    return d[n]
+
+  print(dp(n))
+    

821a0f94a2fe0d259a938628f69ead8253224eac
이건회 <rg970604@naver.com>
2022-03-06 23:12:36 +0900

    Update 백준2667dfsbfs.py

--- 백준 문제모음/백준2667dfsbfs.py
+++ 백준 문제모음/백준2667dfsbfs.py
@@ -16,32 +16,32 @@ dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
 # #dfs풀이
-# def dfs(x,y):
-#   if 0<=x<n and 0<=y<n and graph[x][y]==1:
-#     global cnt
-#     cnt+=1
-#     graph[x][y]=0
-#     for i in range(4):
-#       nx=x+dx[i]
-#       ny=y+dy[i]
-#       if 0>nx or nx>n or 0>ny or ny>n:
-#         continue
-#       dfs(nx,ny)
-#     return True
-#   return False
+def dfs(x,y):
+  if 0<=x<n and 0<=y<n and graph[x][y]==1:
+    global cnt
+    cnt+=1
+    graph[x][y]=0
+    for i in range(4):
+      nx=x+dx[i]
+      ny=y+dy[i]
+      if 0>nx or nx>n or 0>ny or ny>n:
+        continue
+      dfs(nx,ny)
+    return True
+  return False
       
-# cnt=0
-# result=[]
+cnt=0
+result=[]
 
-# for i in range(n):
-#   for j in range(n):
-#     if dfs(i, j)==True:
-#       result.append(cnt)
-#       cnt=0
-# result.sort()
-# print(len(result))
-# for i in result:
-#   print(i)
+for i in range(n):
+  for j in range(n):
+    if dfs(i, j)==True:
+      result.append(cnt)
+      cnt=0
+result.sort()
+print(len(result))
+for i in result:
+  print(i)
       
 # #bfs풀이
 # def bfs(x,y):

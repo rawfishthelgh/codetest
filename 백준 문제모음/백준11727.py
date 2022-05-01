229aec6ec2c9303eb68cd60bbd37dde4ebd1a27a
이건회 <rg970604@naver.com>
2022-05-02 03:06:07 +0900

    11727

---
+++ 백준 문제모음/백준11727.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+def dp(n):
+  d=[0]*1001
+  d[0]=0
+  d[1]=1
+  d[2]=3
+  for i in range(3,n+1):
+    d[i]=d[i-1]+(d[i-2]*2)
+  return d[n]
+print(dp(n)%10007)
+    
